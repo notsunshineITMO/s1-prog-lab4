330b34d3991c1ec415ad57048dbbc7689812eb74
notsunshine <alexsupper75@gmail.com>
2022-12-25 01:28:52 +0300

    [MODIFY] filled CabinOfButton.java added 6 vars and constructor of class, override describe method

--- src/main/java/other/CabinOfButton.java
+++ src/main/java/other/CabinOfButton.java
@@ -1,4 +1,35 @@
 package other;
 
-public class CabinOfButton {
+import technical.IDescribable;
+import technical.PartsOfRocket;
+import technical.Places;
+
+public class CabinOfButton implements IDescribable {
+    private PartsOfRocket location;
+    private Places place;
+    private String door;
+    private String nameplateText;
+    private String table;
+    private String button;
+
+
+    public CabinOfButton (String door, String nameplateText, Places place, PartsOfRocket location){
+        this.door = door;
+        this.nameplateText = nameplateText;
+        this.place = place;
+        this.location = location;
+    }
+
+    public void buttonConditionCheck(boolean isButtonPressed){
+        if (isButtonPressed == true){
+
+        }
+    }
+
+
+    @Override
+    public String describe() {
+        return "Кнопочная кабина находилась " + place + " " + location + ".\n" + "На " + door +  "висела табличка с надписью " + nameplateText +
+                ".\n" + Places.IN + " кнопочной кабине на " + table + " находилась " + button + ".";
+    }
 }
