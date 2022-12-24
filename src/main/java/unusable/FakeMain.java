0f35be41d170e97e43ec9a67ea6d05ce9757fdc2
notsunshine <alexsupper75@gmail.com>
2022-12-24 22:05:08 +0300

    [MODIFY] add very scary construction

--- src/main/java/unusable/FakeMain.java
+++ src/main/java/unusable/FakeMain.java
@@ -1,6 +1,28 @@
 package unusable;
 
 public class FakeMain {
+    private static byte var;
+    public static void giveUp(){}
+    static class InnerClass {
+        static class WhatIsHappening{
+            static class IDontReallyKnow{
+                static class IDontUnderstandAnything{
+                    static class ItsLookLikeAHorror{
+                        static class ImCallingThePolice{
+                            static class KillMeAsFastAsPossible{
+                                public static void why (){
+                                    System.out.println("I'm fine :)");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
     public static void main() {
     }
 }
