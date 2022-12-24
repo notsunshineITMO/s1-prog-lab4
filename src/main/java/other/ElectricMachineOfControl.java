abfb67e8bdbd1fb766690b6858298220aab91de7
notsunshine <alexsupper75@gmail.com>
2022-12-25 01:33:50 +0300

    [ADD] added getter and setter for new var - power

--- src/main/java/other/ElectricMachineOfControl.java
+++ src/main/java/other/ElectricMachineOfControl.java
@@ -1,4 +1,17 @@
 package other;
 
 public class ElectricMachineOfControl {
+    private boolean power;
+
+    public void setPower() {
+
+    }
+
+    public boolean isPower() {
+        return power;
+    }
+
+    public void setPower(boolean power) {
+        this.power = power;
+    }
 }
