ac03a2679c41df5dd37c2c78bd4969ef7b078873
notsunshine <alexsupper75@gmail.com>
2022-12-24 22:09:06 +0300

    [MODIFY] make scary changes

---
+++ src/main/java/di/components/IAppComponent.java
@@ -0,0 +1,6 @@
+//package di.components;
+//import dagger.Component;
+//@Component
+//public interface IAppComponent {
+//    void inject();
+//}
--- src/main/java/other/CabineOfControl.java
+++ src/main/java/other/CabineOfControl.java
@@ -5,7 +5,18 @@ import technical.PartsOfRocket;
 import technical.Places;
 
 public class CabineOfControl {
-
     public CabineOfControl(byte levelOfArmored) {
     }
+
+    private class AntiGravityDevice {
+        public String author;
+        public String material;
+        public double version;
+        public AntiGravityDevice(String author, String material, double version){
+            this.author = author;
+            this.material = material;
+            this.version = version;
+        }
+    }
+
 }
