9651c4a6b1f9725eba57b6a8bde8ee328723a8b0
notsunshine <alexsupper75@gmail.com>
2022-12-26 01:20:58 +0300

    [ADD] add Interested interface

--- src/main/java/person/activities/IInterested.java
+++ src/main/java/person/activities/IInterested.java
@@ -1,5 +1,9 @@
 package person.activities;
 
-public interface IInterested {
+import person.Person;
+import technical.IDescribable;
+import technical.Places;
 
+public interface IInterested {
+    void interested(Places what);
 }
