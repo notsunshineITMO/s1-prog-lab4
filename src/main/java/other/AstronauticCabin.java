d7871306a06c67df19cc6a489120111973656b6b
notsunshine <alexsupper75@gmail.com>
2022-12-25 12:16:36 +0300

    [ADD] added constructor for AstronauticCabin.java

--- src/main/java/other/AstronauticCabin.java
+++ src/main/java/other/AstronauticCabin.java
@@ -4,4 +4,10 @@ public class AstronauticCabin {
     private String telescope;
     private String radioLocator;
     private String interactiveMap;
+
+    public AstronauticCabin (String telescope, String radioLocator, String interactiveMap){
+        this.interactiveMap = interactiveMap;
+        this.radioLocator = radioLocator;
+        this.interactiveMap = interactiveMap;
+    }
 }
