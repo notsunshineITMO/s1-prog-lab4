43ccd97e64aa1e7c10c146f5eeeb5a1a533eccaa
notsunshine <alexsupper75@gmail.com>
2022-12-25 12:47:01 +0300

    [MODIFY] added vars, constructor, getters and setters for CabinOfAnalytics.java

--- src/main/java/Cabins/CabinOfAnalytics.java
+++ src/main/java/Cabins/CabinOfAnalytics.java
@@ -1,5 +1,40 @@
 package Cabins;
 
-public class CabinOfAnalytics {
+import technical.PartsOfRocket;
 
+public class CabinOfAnalytics {
+    private String microscope;
+    private String vials;
+    private String mineralSamples;
+    public final static PartsOfRocket location = PartsOfRocket.UPPER_PART;
+
+    public CabinOfAnalytics(String microscope, String vials, String mineralSamples){
+        this.microscope = microscope;
+        this.vials = vials;
+        this.mineralSamples = mineralSamples;
+    }
+
+    public String getMicroscope() {
+        return microscope;
+    }
+
+    public void setMicroscope(String microscope) {
+        this.microscope = microscope;
+    }
+
+    public String getVials() {
+        return vials;
+    }
+
+    public void setVials(String vials) {
+        this.vials = vials;
+    }
+
+    public String getMineralSamples() {
+        return mineralSamples;
+    }
+
+    public void setMineralSamples(String mineralSamples) {
+        this.mineralSamples = mineralSamples;
+    }
 }
