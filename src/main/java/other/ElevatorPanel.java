5cb70bc7ada426ac4ece8f08c5531362b5a6c43b
notsunshine <alexsupper75@gmail.com>
2022-12-25 14:18:27 +0300

    [ADD] add and set up ElevatorPanel

---
+++ src/main/java/other/ElevatorPanel.java
@@ -0,0 +1,9 @@
+package other;
+
+public interface ElevatorPanel {
+
+    String buttonToUp(boolean value);
+    String buttonToDown(boolean value);
+    String exactlyLevel(int value);
+    String doorsIsOpen(boolean value);
+}
