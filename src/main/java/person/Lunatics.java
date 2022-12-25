eeef60780aeae79d6c49837910270690ea66693a
notsunshine <alexsupper75@gmail.com>
2022-12-25 13:26:47 +0300

    [MODIFY] filled Lunatics.java

--- src/main/java/person/Lunatics.java
+++ src/main/java/person/Lunatics.java
@@ -1,19 +1,37 @@
 package person;
 
 import technical.IDescribable;
+import technical.Where;
 
 public class Lunatics extends Person implements IDescribable {
+    private int count;
+    private String location;
+    private final Where where = new Where();
 
-
-    public Lunatics(String name) {
+    public Lunatics(String name, int count) {
         super(name);
+        this.count = count;
     }
 
+    public void setCount(int count) {
+        this.count = count;
+    }
 
+    public int getCount() {
+        return count;
+    }
 
+    public void setLocation(String whereExactly, String location) {
+        where.setPlace(whereExactly,location);
+        this.location = where.getPlace();
+    }
+
+    public String getLocation() {
+        return location;
+    }
 
     @Override
-    public String describe() {
-        return null;
+    public String describe(){
+        return getCount() + " " + getName() + "находились " + getLocation();
     }
 }
