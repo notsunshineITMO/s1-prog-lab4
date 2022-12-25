9494049f1ebec3fbbcffb7ecdcc891fba16a17cd
notsunshine <alexsupper75@gmail.com>
2022-12-25 13:23:24 +0300

    [ADD] added Lunatics

---
+++ src/main/java/person/Lunatics.java
@@ -0,0 +1,19 @@
+package person;
+
+import technical.IDescribable;
+
+public class Lunatics extends Person implements IDescribable {
+
+
+    public Lunatics(String name) {
+        super(name);
+    }
+
+
+
+
+    @Override
+    public String describe() {
+        return null;
+    }
+}
