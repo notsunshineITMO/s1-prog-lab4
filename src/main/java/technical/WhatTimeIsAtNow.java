b61b8a8239599ca588775aba1d8168702f5d4a05
notsunshine <alexsupper75@gmail.com>
2022-12-25 18:43:49 +0300

    [ADD] add WhatTimeIsAtNow.java

---
+++ src/main/java/technical/WhatTimeIsAtNow.java
@@ -0,0 +1,17 @@
+package technical;
+
+public class WhatTimeIsAtNow {
+    private Time time;
+
+    public WhatTimeIsAtNow(){
+
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+}
