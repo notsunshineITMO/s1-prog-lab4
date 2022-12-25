1c8c543e83dc3038fd1d730f99c91dd50e7181fd
notsunshine <alexsupper75@gmail.com>
2022-12-25 12:24:18 +0300

    [MODIFY] added constructor for CabinOfPhotoAndVideo.java and setters and getters

--- src/main/java/other/CabinOfPhotoAndVideo.java
+++ src/main/java/other/CabinOfPhotoAndVideo.java
@@ -5,4 +5,33 @@ public class CabinOfPhotoAndVideo {
     private String lenses;
     private String softBoxes;
 
+    public CabinOfPhotoAndVideo(String cameras, String lenses, String softBoxes){
+        this.cameras = cameras;
+        this.lenses = lenses;
+        this.softBoxes = softBoxes;
+    }
+
+    public void setCameras(String cameras) {
+        this.cameras = cameras;
+    }
+
+    public void setLenses(String lenses) {
+        this.lenses = lenses;
+    }
+
+    public void setSoftBoxes(String softBoxes) {
+        this.softBoxes = softBoxes;
+    }
+
+    public String getCameras() {
+        return cameras;
+    }
+
+    public String getLenses() {
+        return lenses;
+    }
+
+    public String getSoftBoxes() {
+        return softBoxes;
+    }
 }
